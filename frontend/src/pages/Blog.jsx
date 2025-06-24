a241dba848fedde23e2722fe9389899d4726761f
nihala shirin <nihalashirin02@gmail.com>
2025-06-24 20:03:12 +0530

    commit changes

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+// import React, { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { blog_data } from '../assets/assets';
 
-const Blog = () => {
-  return (
-    <div>
-      <h1>Blog</h1>
-    </div>
-  )
-}
+// const Blog = () => {
+//   const { id } = useParams();
+//   const [data, setData] = useState(null);
 
-export default Blog
+//   const fetchBlogData = async () => {
+//     const data = blog_data.find(item => item.id == id);
+//     setData(data);
+//   };
+
+//   useEffect(() => {
+//     fetchBlogData();
+//   }, [id]); 
+
+//   return data ? (
+//     <div>
+//       <h1>Blog</h1>
+//     </div>
+//   ) : (
+//     <div>Loading</div>
+//   );
+// };
+
+// export default Blog;
