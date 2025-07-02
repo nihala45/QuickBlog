2a5e2580419c53c830aa70359aa04382f3e9ba53
nihala shirin <nihalashirin02@gmail.com>
2025-07-02 17:15:50 +0530

    base url changed

--- frontend/src/utils/token.js
+++ frontend/src/utils/token.js
@@ -1,7 +1,7 @@
 // export const BASE_URL='http://127.0.0.1:8000/';
 
 
-export const BASE_URL= "https://quickblog-9sdh.onrender.com/"
+export const BASE_URL= "https://quickblog-1-87c1.onrender.com"
 
 export const ACCESS_TOKEN='access_token';
 export const REFRESH_TOKEN='refresh_token';
