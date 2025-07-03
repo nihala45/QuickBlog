fb043fad98d7e66c2c43172d6d83a2e1fbdacffa
nihala shirin <nihalashirin02@gmail.com>
2025-07-04 00:41:47 +0530

    http

--- frontend/src/utils/token.js
+++ frontend/src/utils/token.js
@@ -1,7 +1,7 @@
 // export const BASE_URL='http://127.0.0.1:8000/';
 
 
-export const BASE_URL= "https://15.207.55.60:8000/";
+export const BASE_URL= "http://15.207.55.60:8000/";
 
 export const ACCESS_TOKEN='access_token';
 export const REFRESH_TOKEN='refresh_token';
