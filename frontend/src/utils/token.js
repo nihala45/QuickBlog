e04485f1ae0d1ce3f5c179798c715567d2451714
nihala shirin <nihalashirin02@gmail.com>
2025-07-04 11:11:13 +0530

    base url changed on aws

--- frontend/src/utils/token.js
+++ frontend/src/utils/token.js
@@ -1,7 +1,7 @@
 // export const BASE_URL='http://127.0.0.1:8000/';
 
 
-export const BASE_URL= "https://quickblog-9sdh.onrender.com";
+export const BASE_URL= "http://15.207.55.60:8000/";
 
 export const ACCESS_TOKEN='access_token';
 export const REFRESH_TOKEN='refresh_token';
